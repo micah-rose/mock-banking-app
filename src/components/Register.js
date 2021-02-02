75ef95fd474da013a9971a354fe12075efe921aa
Micah Lund <micah.r.lund@gmail.com>
2021-02-01 22:32:01 -0700

    Took same elements from the Login page to create the Register page.

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,136 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Form, Button } from 'react-bootstrap';
+import { validateFields } from '../utils/common';
+import { Link } from 'react-router-dom';
 
-const Register = () => {
-    return <div>Register Page</div>
+class Register extends React.Component{
+    state = {
+        first_name: '',
+        last_name: '',
+        email: '',
+        password: '',
+        cpassword: '',
+        successMsg: '',
+        errorMsg: '',
+        isSubmitted: false
+    }
+
+    registerUser = (event) => {
+        event.preventDefault();
+        const { first_name, last_name, email, password, cpassword} = this.state;
+
+        const fieldsToValidate = [
+            {first_name},
+            {last_name},
+            {email},
+            {password},
+            {cpassword}
+        ]
+
+        const allFieldsEntered = validateFields(fieldsToValidate);
+        if (!allFieldsEntered) {
+            this.setState({
+                errorMsg: {
+                    signup_error: 'Please enter all fields.'
+                }
+            })
+        } else {
+            if (password !== cpassword) {
+                this.setState({
+                    errorMsg: {
+                        signup_error: 'Passwords do not match.'
+                    }
+                })
+            } else {
+                this.setState({ isSubmitted: true })
+            }
+        }
+    }
+
+    handleInputChange = (event) => {
+        const {name, valie} = event.target;
+        this.setState({
+            [name]: value
+        })
+    }
+
+    render() {
+        const { errorMsg, successMsg, isSubmitted } = this.state;
+        return(
+            <div className="login-page">
+                <h2>Register User</h2>
+                <div className="login-form">
+                    <Form onSubmit={this.registerUser}>
+                        {errorMsg && errorMsg.signup_error ? (
+                            <p className="errorMsg centered-message">
+                                {errorMsg.signup_error}
+                            </p>
+                        ) : (
+                            isSubmitted && (
+                                <p className="successMsg centered-message">
+                                    {successMsg}
+                                </p>
+                            )
+                        )}
+                        <Form.Group controlId="first_name">
+                            <Form.Label>First Name</Form.Label>
+                            <Form.Control 
+                                type="text"
+                                name="first_name"
+                                placeholder="Enter First Name"
+                                onChange={this.handleInputChange}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="last_name">
+                            <Form.Label>Last Name</Form.Label>
+                            <Form.Control 
+                                type="text"
+                                name="last_name"
+                                placeholder="Enter Last Name"
+                                onChange={this.handleInputChange}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="email">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control 
+                                type="email"
+                                name="email"
+                                placeholder="Enter Email"
+                                onChange={this.handleInputChange}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="password">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control 
+                                type="password"
+                                name="password"
+                                placeholder="Enter Password"
+                                onChange={this.handleInputChange}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="cpassword">
+                            <Form.Label>Confirm Password</Form.Label>
+                            <Form.Control 
+                                type="password"
+                                name="cpassword"
+                                placeholder="Enter Confirm Password"
+                                onChange={this.handleInputChange}
+                            />
+                        </Form.Group>
+                        <div className="action-items">
+                            <Button variant="primary" type="submit">
+                                Register
+                            </Button>
+                            <Link to="/" className="btn btn-secondary">
+                                Login
+                            </Link>
+                        </div>
+                    </Form>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Register;
