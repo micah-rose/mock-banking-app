55c3c489be02730072be0e3228ee60ec3ed3cbac
Micah Lund <micah.r.lund@gmail.com>
2021-02-06 11:47:12 -0700

    Added content in the try block for field validation and hashing the user password. Also storing user data in the database.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -12,7 +12,45 @@ const Router = express.Router();
 
 Router.post('/signup', async (req, res) => {
     try {
-        //try block
+        const { first_name, last_name, email, password } = req.body;
+        const validFieldsToUpdate = [
+            'first_name',
+            'last_name',
+            'email',
+            'password'
+        ];
+        const receivedFields = Object.keys(req.body);
+
+        const isInvalidFieldProvided = isInvalidField(
+            receivedFields,
+            validFieldsToUpdate
+        );
+
+        if (isInvalidFieldProvided) {
+            return res.status(400).send({
+                signup_error: 'Invalid field.'
+            });
+        }
+
+        const result = await pool.query(
+            'select count(*) as count from bank_user where email=$1',
+            [email]
+        );
+
+        const count = result.rows[0].count;
+        if(count > 0){
+            return res.status(400).send({
+                signup_error: 'User with this email address already exists'
+            })
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 8);
+        await pool.query(
+            'insert into bank_user(first_name, last_name, email, password)',
+            [first_name, last_name, email, hashedPassword]
+        )
+        res.status(201).send();
+
     } catch (error) {
         res.status(400).send({
             signup_error: 'Error while signing up. Try again later.'
