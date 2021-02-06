a1b781f6bbe241d217c2b8152efd45c0430f3800
Micah Lund <micah.r.lund@gmail.com>
2021-02-06 12:04:41 -0700

    Set up the express server and added a start script in package.json to run off the index file.

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
-//index file for backend
+const express = require('express');
+const authRoute = require('./routes/auth');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(authRoute);
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`)
+});
