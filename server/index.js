0c4cc96a25eb899c5cb63316a9557e31633f821f
Micah Lund <micah.r.lund@gmail.com>
2021-02-10 21:10:34 -0700

    Added the use of cors to the index file to allow the app to accept requests from any application.

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const authRoute = require('./routes/auth');
 require('dotenv').config();
 
@@ -6,6 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 app.use(authRoute);
 
 app.listen(PORT, () => {
