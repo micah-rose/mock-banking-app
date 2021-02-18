90b755bcb82988a6115ecc4ed4d08c8b5effd369
Micah Lund <micah.r.lund@gmail.com>
2021-02-17 19:16:31 -0700

    Added the error reducer to the store file.

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,14 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import authReducer from '../reducers/auth';
+import errorsReducer from '../reducers/errors';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
     combineReducers({
-        auth: authReducer
+        auth: authReducer,
+        errors: errorsReducer
     }),
     composeEnhancers(applyMiddleware(thunk))
 )
