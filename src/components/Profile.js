e9090dbcc392eba46f6d4771a52d7a9671b8a090
Micah Lund <micah.r.lund@gmail.com>
2021-02-20 15:51:51 -0700

    Completed JSX for form content and added the matpStateToProps.

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -47,8 +47,52 @@ class Profile extends React.Component {
         const { errorMsg, first_name, last_name, email, isSubmitted } = this.state;
         return (
             <div className="col-md-6 offset-md-3">
-                
+                <Form onSubmit = {this.handleSubmit} className="profile-form">
+                    {errorMsg && errorMsg.update_error ? (
+                        <p className="errorMsg centered-message">{errorMsg.update_error}</p>
+                    ) : (
+                        isSubmitted && (
+                            <p className="successMsg centered-message">
+                                Profile updated successfully.
+                            </p>
+                        )
+                    )}
+                    <Form.Group controlId="email">
+                        <Form.Label>Email Address:</Form.Label>
+                        <span className="label-value">{email}</span>
+                    </Form.Group>
+                    <Form.Group controlId="first_name">
+                        <Form.Label>First Name:</Form.Label>
+                        <Form.Control
+                            type="text"
+                            name="first_name"
+                            placeholder="Enter your first name"
+                            value={first_name}
+                            onChange={this.handleOnChange}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="last_name">
+                        <Form.Label>Last Name:</Form.Label>
+                        <Form.Control
+                            type="text"
+                            name="last_name"
+                            placeholder="Enter your last name"
+                            value={last_name}
+                            onChange={this.handleOnChange}
+                        />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
             </div>
         )
     }
 }
+
+const mapStateToProps = state => ({
+    profile: state.profile,
+    errors: state.errors
+})
+
+export default connect(mapStateToProps)(Profile);
