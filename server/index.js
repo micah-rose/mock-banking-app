df02c102eea7cb1f32586f0306e573506434b7b5
Micah Lund <micah.r.lund@gmail.com>
2021-03-03 18:29:04 -0700

    Forgot to add the route in my index.js file.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const authRoute = require('./routes/auth');
+const profileRoute = require('./routes/profile');
 require('dotenv').config();
 
 const app = express();
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 app.use(authRoute);
+app.use(profileRoute);
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
